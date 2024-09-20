067336c4442059671150ae74797c470ae8ad0512
rems-38 <rem.mazzone@orange.fr>
2024-09-20 18:21:27 +0200

    change to customtkinter

--- classes/ui.py
+++ classes/ui.py
@@ -1,15 +1,39 @@
 # -*- coding: utf-8 -*-
 
-import tkinter as tk
+import customtkinter as tk
 
-class UI(tk.Tk):
+class UI(tk.CTk):
 
     def __init__(self):
         super().__init__()
 
-        self.title("[Degecom] Time Checker")
+        self.title("Time Checker")
         self.geometry("400x400")
+        self.resizable(False, False)
+        tk.set_default_color_theme("dark-blue") # fonctionne pas à voir pk
+        
+    def load(self):
 
-    def init_frame(self):
-        self.frame = tk.Frame(self)
-        self.frame.pack()
+        title = tk.CTkLabel(self, text="Time Checker", font=("Arial", 24))
+
+        sync_button = tk.CTkButton(self, text="Synchroniser l'agenda Google", command=self.sync)
+
+        quit_button = tk.CTkButton(self, text="Quitter", command=self.quit)
+
+        play_button = tk.CTkButton(self, text="▶️", font=("Arial", 24), command=self.play)
+        report_button = tk.CTkButton(self, text="Générer le rapport mensuel", command=self.report)
+
+        title.pack(pady=20)
+        sync_button.pack(pady=20)
+        play_button.pack(pady=20)
+        report_button.pack(pady=20)
+        quit_button.pack(pady=20)
+        
+    def sync(self):
+        print("Synchronisation en cours...")
+
+    def play(self):
+        print("Lecture en cours...")
+
+    def report(self):
+        print("Génération du rapport mensuel...")
--- main.py
+++ main.py
@@ -6,4 +6,5 @@ from classes.ui import UI
 if __name__ == "__main__":
 
     ui = UI()
+    ui.load()
     ui.mainloop()
