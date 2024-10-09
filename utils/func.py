4c0943c6d2f72e2f040de68a1e926d4501dd24e8
rems-38 <rem.mazzone@orange.fr>
2024-10-09 12:11:33 +0200

    change logic to refresh token

--- utils/func.py
+++ utils/func.py
@@ -25,15 +25,15 @@ def google_sync():
     if os.path.exists("token.json"):
         creds = Credentials.from_authorized_user_file("token.json", SCOPES)
 
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
-            creds = flow.run_local_server(port=0)
+    if not creds:
+        flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+        creds = flow.run_local_server(port=0)
+    
+    if creds and (not creds.valid or creds.expired) and creds.refresh_token:
+        creds.refresh(Request())
 
-        with open("token.json", "w") as token:
-            token.write(creds.to_json())
+    with open("token.json", "w") as token:
+        token.write(creds.to_json())
 
     print("Sync Google OK")
     return build("calendar", "v3", credentials=creds)
